b2535d721bf0af5fbf7b0f0ff88cbec43c783877
lightaime <lightaime@gmail.com>
2017-08-02 16:26:01 +0800

    Added MC policy gradient method

---
+++ model/reinforce_model.py
@@ -0,0 +1,137 @@
+import tensorflow as tf
+
+class Model(object):
+    def __init__(self, state_dim, action_dim, entropy_beta=1e-1, optimizer=None):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.entropy_beta = entropy_beta
+        self.optimizer = optimizer or tf.train.RMSPropOptimizer(learning_rate=0.001)
+
+        with tf.name_scope("model_input"):
+            self.input_state = tf.placeholder(tf.float32, shape=[None, self.state_dim], name="states")
+
+        with tf.name_scope("model_target"):
+            self.taken_actions = tf.placeholder(tf.int32, shape=[None, 1], name="taken_actions")
+            self.future_rewards = tf.placeholder(tf.float32, shape=[None, 1], name="future_rewards")
+
+        with tf.name_scope("model"):
+            self.__create_model()
+
+    def __create_policy_network(self):
+        with tf.variable_scope("shared_network"):
+            h1 = tf.layers.dense(self.input_state,
+                                    units=32,
+                                    activation=tf.nn.relu,
+                                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                    name="hidden_1")
+
+            h2 = tf.layers.dense(h1,
+                                    units=32,
+                                    activation=tf.nn.relu,
+                                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                    name="hidden_2")
+
+        with tf.variable_scope("policy_network"):
+            self.policy_outputs = tf.layers.dense(h2,
+                                    units=self.action_dim,
+                                    activation=tf.nn.softmax,
+                                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                    name="policy_outputs")
+
+    def __creat_base_line_network(self):
+        with tf.variable_scope("shared_network"):
+            h1 = tf.layers.dense(self.input_state,
+                                    units=32,
+                                    activation=tf.nn.relu,
+                                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                    name="hidden_1",
+                                    reuse=True)
+
+            h2 = tf.layers.dense(h1,
+                                    units=32,
+                                    activation=tf.nn.relu,
+                                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                    name="hidden_2",
+                                    reuse=True)
+
+        with tf.variable_scope("base_line_network"):
+            self.base_line_outputs = tf.layers.dense(h2,
+                                    units=1,
+                                    activation=None,
+                                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                    name="base_line_outputs")
+
+    def __create_loss(self):
+        with tf.name_scope("compute_policy_gradients"):
+            self.log_probs = tf.log(tf.clip_by_value(self.policy_outputs, 1e-20, 1.0))
+
+            # entropy loss of exploration
+            self.entropy_loss = -tf.reduce_sum(self.policy_outputs * self.log_probs, reduction_indices=1)
+            self.policy_loss = -tf.reduce_sum(tf.multiply(self.log_probs, tf.squeeze(tf.cast(tf.one_hot(self.taken_actions, self.action_dim), tf.float32))) * \
+                                              (self.future_rewards - tf.stop_gradient(self.base_line_outputs)), reduction_indices=1)
+            # self.policy_loss = -tf.reduce_sum(tf.multiply(self.log_probs, tf.squeeze(tf.cast(tf.one_hot(self.taken_actions, self.action_dim), tf.float32))) * \
+                                              # (self.future_rewards), reduction_indices=1)
+            td = self.base_line_outputs - self.future_rewards
+            td = tf.clip_by_value(td, -5.0, 5.0)
+            # self.base_line_loss = tf.nn.l2_loss(self.base_line_outputs - self.future_rewards)
+            self.base_line_loss = tf.reduce_sum(td**2 / 2, reduction_indices=1)
+            self.total_loss = tf.add_n([self.policy_loss,
+                                      self.entropy_beta * self.entropy_loss,
+                                      self.base_line_loss])
+
+    def __create_train_policy_op(self):
+        self.train_policy_op = self.optimizer.minimize(self.policy_loss,
+                                                global_step=tf.contrib.framework.get_global_step())
+
+    def __create_train_op(self):
+        self.train_op = self.optimizer.minimize(self.total_loss,
+                                                global_step=tf.contrib.framework.get_global_step())
+
+    def __create_model(self):
+        self.__create_policy_network()
+        self.__creat_base_line_network()
+        self.__create_loss()
+        self.__create_train_policy_op()
+        self.__create_train_op()
+        self.__create_get_layer_weight_op()
+
+    def predict_policy(self, feed_state, sess=None):
+        sess = sess or tf.get_default_session()
+        return sess.run(self.policy_outputs, {self.input_state: feed_state})
+
+    def update_policy(self, feed_state, feed_taken_actions, feed_future_rewards, sess=None):
+        sess = sess or tf.get_default_session()
+        return sess.run([self. train_policy_op, self.policy_loss],
+                        {self.input_state: feed_state,
+                            self.taken_actions: feed_taken_actions,
+                            self.future_rewards: feed_future_rewards})
+
+    def predict_baseline(self, feed_state, sess=None):
+        sess = sess or tf.get_default_session()
+        return sess.run(self.base_line_outputs, {self.input_state: feed_state})
+
+    def update_baseline(self, feed_state, feed_future_rewards, sess=None):
+        sess = sess or tf.get_default_session()
+        return sess.run(self.base_line_loss, {self.input_state: feed_state,
+                                              self.future_rewards: feed_future_rewards})
+
+    def update(self, feed_state, feed_taken_actions, feed_future_rewards, sess=None):
+        sess = sess or tf.get_default_session()
+        return sess.run([self.train_op, self.total_loss, self.policy_loss, self.base_line_loss],
+                        {self.input_state: feed_state,
+                            self.taken_actions: feed_taken_actions,
+                            self.future_rewards: feed_future_rewards})
+
+    def __create_get_layer_weight_op(self):
+        with tf.name_scope("model"):
+            with tf.variable_scope("shared_network", reuse=True):
+                self.h1_weiget = tf.get_variable("hidden_1/kernel")
+                self.h1_bias = tf.get_variable("hidden_1/bias")
+
+    def run_layer_weight(self, sess=None):
+        sess = sess or tf.get_default_session()
+        return sess.run([self.h1_weiget, self.h1_bias])
+
+
+if __name__ == '__main__':
+    model = Model(40, 5)
--- run_reinforce.py
+++ run_reinforce.py
@@ -1,6 +1,9 @@
 import gym
+from gym import wrappers
 import tensorflow as tf
 import numpy as np
+import random
+from model.reinforce_model import Model
 
 import logging
 
@@ -8,22 +11,84 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+# make environment
 env_name = 'CartPole-v0'
 env = gym.make(env_name)
+env = wrappers.Monitor(env, env_name+"experiment-1", force=True)
 logger.info("{} is made".format(env_name))
 action_dim = env.action_space.n
 state_dim = env.observation_space.shape[0]
 logger.info("action dimension of env is {}".format(action_dim))
 logger.info("state dimension of env is {}".format(state_dim))
 
-for i_episode in xrange(20):
-    observation = env.reset()
-    episode_reward = 0
-    for t in xrange(100):
-        env.render()
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        episode_reward += reward
-        if done:
-            logger.info("Episode finished after {} timesteps with total reward {}".format(t+1, episode_reward))
-            break
+# parameters
+MAX_EPISODE = 100000
+MAX_STEP = 1000
+DISCOUNT_FACTOR = 0.99
+VERBOSE = False
+
+tf.reset_default_graph()
+global_step = tf.Variable(0, name="global_step", trainable=False)
+model = Model(state_dim, action_dim)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    last_100_epi_red = []
+    for i_episode in xrange(MAX_EPISODE):
+        observation = env.reset()
+        episode_reward = 0
+        state_rollout = []
+        action_rollout = []
+        reward_rollout = []
+        done_rollout = []
+        for t in xrange(MAX_STEP):
+            state_rollout.append(observation)
+            # env.render()
+            p=model.predict_policy([observation], sess)
+            action = np.random.choice(action_dim, 1, p=p[0])
+            observation, reward, done, info = env.step(action[0])
+            episode_reward += reward
+
+            action_rollout.append(action)
+            if done and episode_reward != 200:
+                reward = -10
+            elif done and episode_reward == 200:
+                reward = 10
+                print("POSITIVE DONE!")
+            reward_rollout.append(reward)
+            done_rollout.append(done)
+
+            if done:
+                last_100_epi_red.insert(0, episode_reward)
+                if len(last_100_epi_red) > 100:
+                    last_100_epi_red.pop()
+                logger.info("Episode {} finished after {} timesteps with total reward {}".format(i_episode, t+1, episode_reward))
+                avg_reward = sum(last_100_epi_red) / float(len(last_100_epi_red))
+                logger.info("Last 100 episodes average reward is {}".format(sum(last_100_epi_red) / float(len(last_100_epi_red))))
+                if avg_reward >= 195.0:
+                    print("PROBLEM SOLVED!")
+                    exit()
+                break
+
+        w = np.array([])
+        b = np.array([])
+        for i, sard in enumerate(zip(state_rollout, action_rollout, reward_rollout, done_rollout)):
+            s, a, r, d = sard
+            _, total_loss, policy_loss, base_line_loss = model.update([s],
+                            [a],
+                            [[sum(DISCOUNT_FACTOR**i_ * rwd for i_, rwd in enumerate(reward_rollout[i:]))]],
+                            sess)
+            if VERBOSE:
+                if i%10 == 0:
+                    print(base_line_loss)
+                    print(total_loss)
+                w_p, b_p = w.copy(), b.copy()
+                w, b = model.run_layer_weight()
+                if i > 0:
+                    print(w-w_p)
+                    print(b-b_p)
+
+        del state_rollout
+        del action_rollout
+        del reward_rollout
+        del done_rollout
